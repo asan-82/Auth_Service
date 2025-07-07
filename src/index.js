9ebc8662cb959fc6df3674843ba5da0458e3095a
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-07 16:56:08 +0530

    User Signup logic with Password Encryption done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express=require("express");
 const { PORT } = require('../src/config/serverConfig');
+const apiRoutes=require("./routes/index");
 const app=express();
 
 const prepareAndStartServer=()=>{
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use("/api",apiRoutes);
     app.listen(3001,()=>{
         console.log(`Server Started on ${PORT}`);
     })
