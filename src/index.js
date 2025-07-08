779a384c88458d35fd62d3a7f9e085e492583cb3
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-08 10:48:39 +0530

    added logic to check for the incoming plain password with the encrypted password

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const express=require("express");
-const { PORT } = require('../src/config/serverConfig');
+const { PORT, JWT_KEY } = require('../src/config/serverConfig');
 const apiRoutes=require("./routes/index");
+const UserService=require("../src/services/user-service");
+const { verify } = require("jsonwebtoken");
 const app=express();
 
 const prepareAndStartServer=()=>{
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use("/api",apiRoutes);
+    
     app.listen(3001,()=>{
         console.log(`Server Started on ${PORT}`);
-    })
+      /*  const service=new UserService();
+    const newToken=service.createToken({email:"aarushi@admin.com",id:1});
+   // console.log(newToken);
+const response=verify(newToken,JWT_KEY);
+console.log(response);
+*/    
+})
 
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,9 @@
 const UserRepository=require("../repository/user-repository");
+const jwt=require("jsonwebtoken");
+const bcrypt=require("bcrypt");
+const {JWT_KEY}=require("../config/serverConfig");
+const { use } = require("react");
+
 
 class UserService{
     constructor()
@@ -19,6 +24,45 @@ class UserService{
             throw error;
         }
     }
+    createToken(user)
+    {
+        try{
+            const result=jwt.sign(user,JWT_KEY,{expiresIn:'1h'})
+            return result;
+
+        }
+        catch(error)
+        {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+    checkPassword(userInputPlainPassword,encryptedPassword)
+    {
+        try{
+            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+
+        }
+        catch(error)
+        {
+            console.log("Something went wrong in password comparison");
+            throw error;
+        }
+    }
+    verifyToken(token)
+    {
+        try{
+            const response=jwt.verify(token,JWT_KEY);
+            return response;
+
+        }
+        catch(error)
+        {
+            console.log("Something went wrong in token validation",error);
+            throw error;
+        }
+    }
 }
+
 
 module.exports=UserService;
