ac7e843b29c19c89a3f9c3339c60326a3ddc5a40
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-09 01:25:25 +0530

    added roles and user roles tables

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const { PORT, JWT_KEY } = require('../src/config/serverConfig');
 const apiRoutes=require("./routes/index");
 const UserService=require("../src/services/user-service");
+const db=require("../src/models/index");
+const {User,Role}=require("../src/models/index");
 const { verify } = require("jsonwebtoken");
 const app=express();
 
@@ -10,8 +12,16 @@ const prepareAndStartServer=()=>{
     app.use(express.urlencoded({ extended: true }));
     app.use("/api",apiRoutes);
     
-    app.listen(3001,()=>{
+    app.listen(3001,async ()=>{
         console.log(`Server Started on ${PORT}`);
+       /* if(process.env.DB_SYNC)
+        {
+          db.sequelize.sync({alter:true});
+        }
+          */
+         const u=await User.findByPk(3);
+         const r=await Role.findByPk(1);
+         u.addRole(r);
       /*  const service=new UserService();
     const newToken=service.createToken({email:"aarushi@admin.com",id:1});
    // console.log(newToken);
